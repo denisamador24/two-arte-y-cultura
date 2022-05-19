2469f27f565cad4fdd2e464efea5f695d0d9a243
Denis <denisnica.242004@gmail.com>
2022-05-19 11:58:59 -0600

    reconstrucion del proyecto y navigationDawer hecho

---
+++ app/src/main/java/com/example/arteycultura/InfoGrupActivity.kt
@@ -0,0 +1,11 @@
+package com.example.arteycultura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoGrupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_grup)
+    }
+}
